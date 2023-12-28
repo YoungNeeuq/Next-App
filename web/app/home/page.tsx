7df7c789bb7610c5481761baba673cf9c2b1237e
YoungNeeuq <haulv.dev@gmail.com>
2023-12-28 09:07:49 +0700

    first commit

---
+++ web/app/home/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from 'web/ui-components/src/components/header';
+import Footer from 'web/ui-components/src/components/footer';
+import Contact from 'web/ui-components/src/components/Contact';
+const page = () => {
+  return (
+    <div>
+      <Header />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default page;
---
+++ web/app/page.tsx
@@ -0,0 +1,5 @@
+export default async function Index() {
+  return (
+    <div className="w-full h-full text-9xl font-bold text-center">Heloo</div>
+  );
+}
